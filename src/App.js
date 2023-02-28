f9033d092f08e78e8ec5326fff0eaa6611599753
Omar <vip.mergeshev@gmail.com>
2023-02-28 11:41:50 +0600

    add homeworks5

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
+import { useForm } from "react-hook-form";
 
+function App(props) {
+  const { register, handleSubmit } = useForm();
 
-function App() {
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
+  const status = 'Регистрация'
+
   return (
-    <div className="App">
-    hhhhhhhhh
-    </div>
+    <>
+    <div className='status'>{status}</div>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <p>Name:<input type="text" id="name" {...register("name", { required: true })} /></p>
+      <p> Email:<input
+        type="email"
+        id="email"
+        {...register("email", { required: true, pattern: /^\S+@\S+$/i })}
+      /></p>
+      <p>Number:<input
+        type="number"
+        id="age"
+        {...register("age", { required: true, min: 18, max: 99 })}
+      /> </p>
+      <p>Possword:<input
+        type="password"
+        id="password"
+        {...register("password", { required: true, minLength: 8 })}
+      /> </p>
+      <input type="submit" />
+    </form>
+    </> 
   );
 }
 
